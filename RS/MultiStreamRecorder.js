898a476936265e575821ee0843dd3609202008a8
Vladyslav Charuk <vlad.charu@gmail.com>
2022-05-10 18:54:30 +0300

    first

---
+++ RS/MultiStreamRecorder.js
@@ -0,0 +1,192 @@
+function MultiStreamRecorder(arrayOfMediaStreams, options) {
+    arrayOfMediaStreams = arrayOfMediaStreams || [];
+    var self = this;
+
+    var mixer;
+    var mediaRecorder;
+
+    options = options || {
+        mimeType: 'video/webm',
+        video: {
+            width: 360,
+            height: 240
+        }
+    };
+
+    if (!options.frameInterval) {
+        options.frameInterval = 10;
+    }
+
+    if (!options.video) {
+        options.video = {};
+    }
+
+    if (!options.video.width) {
+        options.video.width = 360;
+    }
+
+    if (!options.video.height) {
+        options.video.height = 240;
+    }
+
+    /**
+     * This method records all MediaStreams.
+     * @method
+     * @memberof MultiStreamRecorder
+     * @example
+     * recorder.record();
+     */
+    this.record = function() {
+        mixer = new MultiStreamsMixer(arrayOfMediaStreams);
+
+        if (getAllVideoTracks().length) {
+            mixer.frameInterval = options.frameInterval || 10;
+            mixer.width = options.video.width || 360;
+            mixer.height = options.video.height || 240;
+            mixer.startDrawingFrames();
+        }
+
+        if (options.previewStream && typeof options.previewStream === 'function') {
+            options.previewStream(mixer.getMixedStream());
+        }
+
+        // record using MediaRecorder API
+        mediaRecorder = new MediaStreamRecorder(mixer.getMixedStream(), options);
+        mediaRecorder.record();
+    };
+
+    function getAllVideoTracks() {
+        var tracks = [];
+        arrayOfMediaStreams.forEach(function(stream) {
+            getTracks(stream, 'video').forEach(function(track) {
+                tracks.push(track);
+            });
+        });
+        return tracks;
+    }
+
+    /**
+     * This method stops recording MediaStream.
+     * @param {function} callback - Callback function, that is used to pass recorded blob back to the callee.
+     * @method
+     * @memberof MultiStreamRecorder
+     * @example
+     * recorder.stop(function(blob) {
+     *     video.src = URL.createObjectURL(blob);
+     * });
+     */
+    this.stop = function(callback) {
+        if (!mediaRecorder) {
+            return;
+        }
+
+        mediaRecorder.stop(function(blob) {
+            self.blob = blob;
+
+            callback(blob);
+
+            self.clearRecordedData();
+        });
+    };
+
+    /**
+     * This method pauses the recording process.
+     * @method
+     * @memberof MultiStreamRecorder
+     * @example
+     * recorder.pause();
+     */
+    this.pause = function() {
+        if (mediaRecorder) {
+            mediaRecorder.pause();
+        }
+    };
+
+    /**
+     * This method resumes the recording process.
+     * @method
+     * @memberof MultiStreamRecorder
+     * @example
+     * recorder.resume();
+     */
+    this.resume = function() {
+        if (mediaRecorder) {
+            mediaRecorder.resume();
+        }
+    };
+
+    /**
+     * This method resets currently recorded data.
+     * @method
+     * @memberof MultiStreamRecorder
+     * @example
+     * recorder.clearRecordedData();
+     */
+    this.clearRecordedData = function() {
+        if (mediaRecorder) {
+            mediaRecorder.clearRecordedData();
+            mediaRecorder = null;
+        }
+
+        if (mixer) {
+            mixer.releaseStreams();
+            mixer = null;
+        }
+    };
+
+    /**
+     * Add extra media-streams to existing recordings.
+     * @method
+     * @memberof MultiStreamRecorder
+     * @param {MediaStreams} mediaStreams - Array of MediaStreams
+     * @example
+     * recorder.addStreams([newAudioStream, newVideoStream]);
+     */
+    this.addStreams = function(streams) {
+        if (!streams) {
+            throw 'First parameter is required.';
+        }
+
+        if (!(streams instanceof Array)) {
+            streams = [streams];
+        }
+
+        arrayOfMediaStreams.concat(streams);
+
+        if (!mediaRecorder || !mixer) {
+            return;
+        }
+
+        mixer.appendStreams(streams);
+    };
+
+    /**
+     * Reset videos during live recording. Replace old videos e.g. replace cameras with full-screen.
+     * @method
+     * @memberof MultiStreamRecorder
+     * @param {MediaStreams} mediaStreams - Array of MediaStreams
+     * @example
+     * recorder.resetVideoStreams([newVideo1, newVideo2]);
+     */
+    this.resetVideoStreams = function(streams) {
+        if (!mixer) {
+            return;
+        }
+
+        if (streams && !(streams instanceof Array)) {
+            streams = [streams];
+        }
+
+        mixer.resetVideoStreams(streams);
+    };
+
+    // for debugging
+    this.name = 'MultiStreamRecorder';
+    this.toString = function() {
+        return this.name;
+    };
+}
+
+if (typeof RS !== 'undefined') {
+    RS.MultiStreamRecorder = MultiStreamRecorder;
+}
